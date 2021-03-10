fe7840ae055532e1325ee6de4ab93b3d8f946556
Heartsh <heartsh@heartsh.io>
2021-03-10 19:20:55 +0900

    Remove test feature in order to compile by stable release channel

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,3 @@
-#![feature(test)]
 //! This package contains an implementation of
 //! [BFGS](https://en.wikipedia.org/w/index.php?title=BFGS_method), an algorithm for minimizing
 //! convex twice-differentiable functions.
